fe22ee13a044712d64a79657d73dafaaf2b11f67
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2024-03-28 11:36:42 -0400

    feat: implement a scoreboard to keep score for both players

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
-from ball import Ball
-from paddle import Paddle
+from actors.ball import Ball
+from actors.paddle import Paddle
+from actors.scoreboard import Scoreboard
 import time
 
 screen  = Screen()
@@ -12,8 +13,8 @@ screen.tracer(0)
 
 l_paddle = Paddle(-350, 0)
 r_paddle = Paddle(350, 0)
-
 ball = Ball(0, 0)
+scoreboard = Scoreboard()
 
 screen.listen()
 
@@ -28,5 +29,21 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
     ball.move()
+
+    # Detect collision with top and bottom wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with the Paddles
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    
+    if ball.xcor() > 380:
+        scoreboard.l_point()
+        ball.reset()
+
+    if ball.xcor() < -380:
+        scoreboard.r_point()
+        ball.reset()
 
 screen.exitonclick()
