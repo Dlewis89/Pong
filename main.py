bcf6c44fda8c1faf984c4620e6edf6f580ded609
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2024-03-28 11:59:30 -0400

    chore: add comments and requirements.txt for good practice even though no dependencies

--- main.py
+++ main.py
@@ -4,26 +4,31 @@ from actors.paddle import Paddle
 from actors.scoreboard import Scoreboard
 import time
 
+# Scren Setup
 screen  = Screen()
-
 screen.bgcolor('black')
 screen.setup(width=800, height=600)
 screen.title('Pong')
 screen.tracer(0)
 
+# Initialing Actors
 l_paddle = Paddle(-350, 0)
 r_paddle = Paddle(350, 0)
 ball = Ball(0, 0)
 scoreboard = Scoreboard()
 
+# Screen Events
 screen.listen()
 
+# Left Paddle Controls
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
+# Right Paddle Controls
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
 
+# Game loop
 game_is_on = True
 while game_is_on:
     time.sleep(ball.move_speed)
@@ -38,12 +43,15 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
     
+    # Detects if left paddle scored
     if ball.xcor() > 380:
         scoreboard.l_point()
         ball.reset()
 
+    # Detects if left paddle scored
     if ball.xcor() < -380:
         scoreboard.r_point()
         ball.reset()
 
+# Closes the game
 screen.exitonclick()
