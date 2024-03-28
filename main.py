ce255d07653f6181f200c8a90c14bfc1d75a2b2f
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2024-03-28 10:16:44 -0400

    feat: get paddles and ball object on the screen

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from turtle import Screen
+from ball import Ball
+from paddle import Paddle
+import time
 
 screen  = Screen()
 
 screen.bgcolor('black')
 screen.setup(width=800, height=600)
-
 screen.title('Pong')
+screen.tracer(0)
+
+l_paddle = Paddle(-350, 0)
+r_paddle = Paddle(350, 0)
+
+ball = Ball(0, 0)
+
+screen.listen()
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
 
 screen.exitonclick()
