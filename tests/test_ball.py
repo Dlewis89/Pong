1b607aea35f364696a13fdc754b90c7950695d88
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2024-03-28 20:44:13 -0400

    tests: better tests for the ball class

--- tests/test_ball.py
+++ tests/test_ball.py
@@ -4,25 +4,37 @@ from actors.ball import Ball
 class TestBallMethods(unittest.TestCase):
 
     def setUp(self):
-        self.ball = Ball()
+        self.ball = Ball(10, 10)
 
     def test_ball_move(self):
+        ball_x = self.ball.xcor()
+        ball_y = self.ball.ycor()
+
         self.ball.move()
 
-        self.assertEqual(self.ball.xcor(), 10)
-        self.assertEqual(self.ball.ycor(), 10)
+        self.assertEqual(self.ball.xcor(), ball_x + self.ball.x_move)
+        self.assertEqual(self.ball.ycor(), ball_y + self.ball.y_move)
     
     def test_ball_bounce_x(self):
+
+        ball_x_move = self.ball.x_move
+
         self.ball.bounce_x()
 
-        self.assertEqual(self.ball.x_move, -10)
+        self.assertEqual(self.ball.x_move, -ball_x_move)
 
     def test_ball_bounce_y(self):
+        ball_y_move = self.ball.y_move
+
         self.ball.bounce_y()
 
-        self.assertEqual(self.ball.y_move, -10)
+        self.assertEqual(self.ball.y_move, -ball_y_move)
 
     def test_ball_reset(self):
+
+        ball_x = self.ball.xcor()
+        ball_y = self.ball.ycor()
+
         self.ball.reset()
 
         self.assertEqual(self.ball.xcor(), 0)
